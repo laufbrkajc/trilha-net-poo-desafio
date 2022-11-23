785a3391e34e38cbc3a5f76b6074b873999d574b
Gabriel <podryto@protonmail.com>
2022-11-23 01:20:46 -0300

    Implementada a funcionalidade de listar o smartphones cadastrados e começo do trabalho em Chamar().

--- Program.cs
+++ Program.cs
@@ -1,11 +1,10 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
-Iphone[] iphones = new Iphone[0];
-Nokia[] nokias = new Nokia[0];
+List<Smartphone> smartphones = new List<Smartphone>();
 char opcaoMenu = ' ';
 
-while (opcaoMenu != 's'&& opcaoMenu != 'S')
+while (opcaoMenu != 's' && opcaoMenu != 'S')
 {
     Console.WriteLine("O que deseja fazer?");
     Console.WriteLine("Adicionar um novo telefone, Ligar para um número, Remover um telefone, Instalar um aplicativo ou Sair?(A/L/I/S)");
@@ -14,22 +13,51 @@ while (opcaoMenu != 's'&& opcaoMenu != 'S')
 
     switch (opcaoMenu)
     {
-        case 'a' or 'A':
+        case 'a' or 'A': // Adicionar um telefone
             AdicionarTelefone();
             break;
-        case 'l' or 'L':
+        case 'c' or 'C': // Chamar um número
             break;
-        case 'r' or 'R':
+        case 'l' or 'L': // Listar os telefones
+            ListarSmartphones();
             break;
-        case 'i' or 'I':
+        case 'i' or 'I': // Instalar um aplicativo
             break;
-        case 'e' or 'E':
+        case 'e' or 'E': // Excluir um telefone
             break;
-        case 's' or 'S':
+        case 's' or 'S': // Sair
             break;
         default:
             break;
     }
+}
+
+void ListarSmartphones()
+{
+    foreach (var telefone in smartphones)
+    {
+        Console.WriteLine("teste");
+
+        // Começamos imprimindo um rótulo indicando se o telefone é iPhone ou Nokia.
+        Console.WriteLine($"  {0}", (telefone is Iphone) ? "iPhone" : "Nokia");
+        Console.WriteLine($"    {telefone.Numero}");
+        Console.WriteLine($"    {telefone.Modelo}");
+        Console.WriteLine($"    {telefone.IMEI}");
+        Console.WriteLine($"    {telefone.Memoria}");
+    }
+}
+
+void Chamar(string meuNum, string contato)
+{
+    foreach (var telefone in smartphones)
+    {
+        if (telefone.Numero == contato)
+        {
+            telefone.ReceberLigacao(meuNum);
+            return;
+        }
+    }
+
 }
 
 void AdicionarTelefone()
@@ -39,20 +67,26 @@ void AdicionarTelefone()
     string modelo = string.Empty;
     int memoria = 0;
 
-    while (opcao != 's' && opcao != 'S') {
+    while (opcao != 's' && opcao != 'S')
+    {
         Console.WriteLine("Adicionar iPhone ou Nokia? 'S' para sair.(i/n/s)");
         opcao = (char)Console.Read();
         Console.Read();
 
         if (opcao == 's' || opcao == 'S') return;
 
-        if (opcao == 'i' || opcao == 'I') {
+        if (opcao == 'i' || opcao == 'I')
+        {
             ColetaDados(ref num, ref modelo, ref memoria);
-            iphones.Append(new Iphone(num, Guid.NewGuid().ToString(), modelo, memoria));
-        } else if (opcao == 'n' || opcao == 'N') {
+            smartphones.Add(new Iphone(num, Guid.NewGuid().ToString(), modelo, memoria));
+        }
+        else if (opcao == 'n' || opcao == 'N')
+        {
             ColetaDados(ref num, ref modelo, ref memoria);
-            nokias.Append(new Nokia(num, Guid.NewGuid().ToString(), modelo, memoria));
-        } else {
+            smartphones.Add(new Nokia(num, Guid.NewGuid().ToString(), modelo, memoria));
+        }
+        else
+        {
             Console.WriteLine($"{opcao}, não é uma opção válida");
             break;
         }
